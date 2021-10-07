6203b6d655e8caad68570e503750f485e646edfa
Ri Liu <me@ri.id.au>
2021-10-07 11:42:50 +1100

    uncomment siteurl

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    // siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://www.yourdomain.tld",
     title: "Generative Art Archive",
   },
   plugins: [
