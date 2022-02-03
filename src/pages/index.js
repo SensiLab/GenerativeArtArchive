d6928a4d6123b2eb5c2705e4ca17beedd115d93c
Ri Liu <me@ri.id.au>
2022-02-03 14:40:54 +1100

    center align layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,17 +9,21 @@ const Index = ({ data }) => {
     <main className="homepage">
       <title>Home Page</title>
       <header>
-        <h1>Generative Systems Archive</h1>
+        <div class="full-inner">
+          <h1>Generative Systems Archive</h1>
+        </div>
       </header>
       <section>
         <h1>A home for generative systems.</h1>
         <p>An online resource where creative practioners, designers, students and researches interested in generative systems and methods can find information about existing generative systems, their cultural and technical contexts.</p>
       </section>
-      <section className="featured">
-        <h2>Featured article</h2>
-        <h1>Randomness</h1>
-        <p>Randomness is a concept that is well integrated into our common speech, however it escapes unequivocal definitions. The word random is commonly used to describe either things that happen by chance –unexpectedly or with no discernible cause–, or to characterise collections or series of things that don’t seem to have any discernible pattern or order.</p>
-        <a href="/entries/concepts/randomness/">Start reading</a>
+      <section className="featured full">
+        <div className="full-inner">
+          <h2>Featured article</h2>
+          <h1>Randomness</h1>
+          <p>Randomness is a concept that is well integrated into our common speech, however it escapes unequivocal definitions. The word random is commonly used to describe either things that happen by chance –unexpectedly or with no discernible cause–, or to characterise collections or series of things that don’t seem to have any discernible pattern or order.</p>
+          <a href="/entries/concepts/randomness/">Start reading</a>
+        </div>
       </section>
       <section>
         <h1>Explore generative systems</h1>
