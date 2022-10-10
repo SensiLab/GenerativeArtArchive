bc7410db24e8f3493fcd1a1bf68d61d4c6209be6
Ri Liu <me@ri.id.au>
2022-10-10 13:48:27 +1100

    autofocus search input

--- src/components/search-box.js
+++ src/components/search-box.js
@@ -1,15 +1,17 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { useFlexSearch } from 'react-use-flexsearch'
 import { navigate, useStaticQuery, graphql } from "gatsby"
 import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
-import { Autocomplete } from '@mui/material';
-import { TextField } from '@mui/material';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
 import IconButton from '@mui/material/IconButton';
+import SearchResults from './search-results';
 
 const SearchBox = () => {
     const [query, setQuery] = useState('')
-    const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = useState(false);
     const searchData = useStaticQuery(
         graphql`
         {
@@ -37,18 +39,20 @@ const SearchBox = () => {
     const goToSearch = (_e, value) => {
         // _e.preventDefault()
         setQuery(value[0])
-        console.log(value[0])
-        navigate('/search?search='+value[0])
+        // console.log(value[0])
+        // navigate('/search?search='+value[0])
     }
 
-    const dialogOpen = () => setOpen(true);
+    const dialogOpen = () => {
+        setOpen(true);
+    }
     const dialogClose = () => setOpen(false);
 
     return (
-        <div>
+        <div className="search">
             <button onClick={dialogOpen}>Search</button>
-            <Dialog open={open} onClose={dialogClose}>
-                <DialogContent>
+            <Dialog open={open} onClose={dialogClose} fullWidth>
+                <DialogTitle>
                     <IconButton
                         aria-label="close"
                         onClick={dialogClose}
@@ -66,8 +70,20 @@ const SearchBox = () => {
                         getOptionLabel={o => o[0]}
                         onInputChange={(_e, value) => {setQuery(value)}}
                         onChange={goToSearch}
-                        renderInput={(params) => <TextField {...params} label="Search" />}
+                        sx = {{
+                            marginTop: 6
+                        }}
+                        renderInput={(params) =>
+                            <TextField
+                                {...params}
+                                label="Search"
+                                autoFocus
+                            />
+                        }
                     />
+                </DialogTitle>
+                <DialogContent>
+                    <SearchResults searchTerm={query}/>
                 </DialogContent>
             </Dialog>
         </div>
